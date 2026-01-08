99ff15d0bca6250473ae9f63638aa2db0b713253
allenres <allen.resulidze@gmail.com>
2026-01-08 14:40:16 -0800

    genereate tests template

---
+++ src/MyStringListTest.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.Test;
+
+public class MyStringListTest {
+    @Test
+    void testAdd() {
+
+    }
+
+    @Test
+    void testCapacity() {
+
+    }
+
+    @Test
+    void testGet() {
+
+    }
+
+    @Test
+    void testRemove() {
+
+    }
+
+    @Test
+    void testSet() {
+
+    }
+
+    @Test
+    void testSize() {
+
+    }
+}
